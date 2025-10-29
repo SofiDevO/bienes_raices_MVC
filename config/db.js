61d48ecadc446ee3fd2b7d0e71ab57f1ff2bbb5f
SofiDev <angiemeow89@gmail.com>
2025-10-29 01:05:46 -0600

    🚧 Ajustes de clases en debian

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import dotenv from'dotenv';
 import { Sequelize } from "sequelize";
-
 dotenv.config({ path: '.env' });
+
 const db = new Sequelize( process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,{
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
